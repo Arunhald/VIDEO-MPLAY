5f9973caf24f4e3ac0b9a30afb08c7b5a3410d81
levina <82658782+levina-lab@users.noreply.github.com>
2022-03-01 09:57:30 +0700

    [check] none aliases name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from pytgcalls import idle
-from pyrogram.types import Message
+from pyrogram.types import Message as message
 from pyrogram.errors import BotInlineDisabled
 
 from program import LOGS
